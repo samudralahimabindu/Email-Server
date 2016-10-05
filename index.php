dfba34e7e908f7fc26c12b8d686ceda470020583
samudralahimabindu <himabindu30@gmail.com>
2016-10-05 20:26:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body  align="center">
+<div class="container">
+<h1 align="center">E-MAIL SERVER</h1>
+Sign in to Continue<br><br>
+<iframe src="index1.php" width="400px" height="250px"></iframe><br><br>
+<a href="new.php">Create new account</a><br><br>
+<a href="aboutus.php">About Us</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<a href="contacts.php">Contact</a>
+</div>
+</body>
+</html>
+<?php
+?>
